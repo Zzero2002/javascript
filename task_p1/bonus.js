452f3b0e4d13099a097a113e0a6ed7c4b3d391ce
Zzero2002 <ahmedfeso2000@gmail.com>
2022-08-23 13:30:24 +0200

    adding bonus project

---
+++ task_p1/bonus.js
@@ -0,0 +1,15 @@
+var firstnum =  parseInt(prompt("first number"));
+var secondnum = parseInt(prompt("second number"));
+var operation = "+";
+
+if (operation == "+"){
+   var result =  firstnum + secondnum;
+   alert("the summation of firstnum and secondnum = "+result);
+   var resultx = result +result;
+
+   if(result){
+   while(confirm(resultx)){
+    alert(resultx += resultx)
+   }}
+}
+   
