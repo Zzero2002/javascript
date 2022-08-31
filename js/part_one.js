0ee1caa6af10bda52b962343c96cb4b525e2a706
Zzero2002 <ahmedfaisaly2000@gmail.com>
2022-08-31 16:37:13 +0200

    4th project

---
+++ js/part_one.js
@@ -0,0 +1,9 @@
+function xyx(){
+    var x = document.getElementById("text").value
+    var y = document.getElementById("password").value
+    if(x=="admin" && y =="123"){
+        alert("welcome" + x);
+        console.log("welcome"+x)
+    }
+}
+// alert("welcome")
---
+++ js/part_two.js
@@ -0,0 +1,62 @@
+
+
+function checky(){
+    document.getElementsByClassName("list");
+    var green = document.getElementsByClassName("lists1")[0];
+    green.style.background = "green";
+    green.style.border = "2px solid black";
+    
+}
+function deleted(){
+    var red = document.getElementsByClassName("lists1")[0];
+    red.parentNode.removeChild(red)
+}
+function checky2(){
+    document.getElementsByClassName("list");
+    var green = document.getElementsByClassName("lists2")[0];
+    green.style.background = "green";
+    green.style.border = "2px solid black";
+    
+}
+function deleted2(){
+        document.getElementsByClassName("list");
+    var red = document.getElementsByClassName("lists2")[0];
+    red.parentNode.removeChild(red)
+}
+function checky3(){
+    document.getElementsByClassName("list");
+    var green = document.getElementsByClassName("lists3")[0];
+    green.style.background = "green";
+    green.style.border = "2px solid black";
+    
+}
+function deleted3(){
+    document.getElementsByClassName("list");
+    var red = document.getElementsByClassName("lists3")[0];
+    red.parentNode.removeChild(red)
+}
+
+
+function function1() {
+    var ul = document.getElementById("first");
+    var lio = document.getElementsByTagName("li");
+    var button1 = document.createElement("button");
+    var button2 = document.createElement("button");
+    button1.appendChild(document.createTextNode("&#128151;"))
+    button2.appendChild(document.createTextNode("&#128514;"))
+    var li = document.createElement("li");
+    var x = document.getElementById("text").value
+    li.appendChild(document.createTextNode(x));
+    ul.appendChild(li);
+    button1.setAttribute("class","bt1");
+    button2.setAttribute("class","bt2");
+    button1.style.float="right";
+    button2.style.float="right";
+
+    li.appendChild(button1);
+    li.appendChild(button2);
+    li.setAttribute("class", "lists4");
+
+  }
+
+
