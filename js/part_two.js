4aded572420f71231c9093f5edf1707bb16a6ccb
Zzero2002 <ahmedfaisaly2000@gmail.com>
2022-09-01 19:23:02 +0200

    edited

--- js/part_two.js
+++ js/part_two.js
@@ -39,37 +39,33 @@ function deleted3(){
 
 function function1() {
     var ul = document.getElementById("first");
-    var lio = document.getElementsByTagName("li");
     var button1 = document.createElement("button");
     var button2 = document.createElement("button");
-    button1.appendChild(document.createTextNode("&#128151;"))
-    button2.appendChild(document.createTextNode("&#128514;"))
+    button1.appendChild(document.createTextNode("Done"))
+    button2.appendChild(document.createTextNode("Remove"))
     var li = document.createElement("li");
     var x = document.getElementById("text").value
     li.appendChild(document.createTextNode(x));
     ul.appendChild(li);
     button1.classList.add("bt1");
     button2.classList.add("bt2");
-    button1.style.float="right";
-    button2.style.float="right";
-
     li.appendChild(button1);
     li.appendChild(button2);
     li.setAttribute("class", "lists4");
 
   }
 
-function deleted4(){
-    document.getElementsByClassName("list");
-var red = document.getElementsByClassName("lists4")[0];
-red.parentNode.removeChild(red)
+
+document.addEventListener("click" , function(e){
+if(e.target.className == 'bt2'){
+    e.target.parentNode.remove();
 }
-function checky4(){
-document.getElementsByClassName("list");
-var green = document.getElementsByClassName("lists4")[0];
-green.style.background = "green";
-green.style.border = "2px solid black";
+if(e.target.className == 'bt1'){
+    var x = document.getElementsByClassName("lists4")
+    e.target.parentNode.style.background = 'green';
+    e.target.parentNode.style.border = '2px solid black';
 }
+})
 
 
 
