1c51582f5d44ab177ced0c9a46c4c18f33e20a74
Zzero202 <ahmedfaisaly2000@gmail.com>
2022-09-13 10:40:53 +0200

    1st project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// var firstName = prompt("your first name");
+// var Lastname = prompt("your Last name");
+// alert("your name is " + firstName + " " + Lastname);
+// var age = prompt("your age is?");
+// alert("your name is " + firstName + " " + Lastname + " and i have " + age);
+// var x = alert("it’s the first release of a calculator that only has a summation feature.")
+
+// var num1 = parseInt(prompt("first numnber"));
+// var num2 = parseInt(prompt("second number"));
+// var num3 = num1 + num2;
+// alert(num1 + " + " + num2 + " = " + num3 );
+
